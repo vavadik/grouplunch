c14f88a842fd19c4bb86baf4e384c4f5a56ae67e
Vadim Galanov <vgalanov@ascendix.com>
2019-10-20 15:26:17 +0300

    rename fix

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    configureWebpack: config => {
-        if (process.env.NODE_ENV === 'production') {
-            config.resolve.extensions = ['*', '.js', '.vue', '.json'];
-        } else {
-            config.resolve.extensions = ['*', '.js', '.vue', '.json'];
-        }
-    }
-}
